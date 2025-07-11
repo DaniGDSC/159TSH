ff5e4eda37f740b84b1165a56040f25784ed1d0d
DaniGDSC <gdschanu.hieund1601@gmail.com>
2025-07-11 13:30:54 +0700

    qr

--- qrcode.py
+++ qrcode.py
@@ -1,9 +1,9 @@
 import segno
 
-def generate_qr_segno(url: str, output_file: str = "qrcode.svg") -> None:
+def generate_qr_segno(url: str, output_file: str = "qrcode.png") -> None:
     qr = segno.make(url)
     qr.save(output_file, scale=5)
     print(f"✅ QR code saved as {output_file}")
 
 # Example usage
-generate_qr_segno("https://docs.google.com/forms/d/e/1FAIpQLScLQ2y167XiyYEKSJFvuV2MON6z2eNALMZwPKMpnphdSlRYGw/viewform?hl=vi")
+generate_qr_segno("http://54.179.218.141/")
